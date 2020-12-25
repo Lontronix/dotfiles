de8e15e15dcd6bd937385287f61ef31a8d91bc7f
Lonnie Gerol <lonnie@lonniegerol.com>
2020-12-24 20:03:37 -0500

    Added changes common to all computers.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Dotfiles
+
+Configuration files that I use on my computers.
+
+## Installation Guide
+### hostname
+- create a file called .hostname and set the HOSTNAME enviorment variable.
+
+		export HOSTNAME="<hostname>"
+
+### Git
+- On the new machine created ~/.gitconfig if it does not already exist. Add the     following:
+
+		[user]
+			name = Lonnie Gerol
+			email = ...
+		[include]
+			path = ~/dotfiles/git/gitconfig
---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm -f ~/.p10k.zsh 						     # removing zshrc if it exists
+ln -sf ~/dotfiles/zsh/p10k.zsh ~/.p10k.zsh   # powerline customization file
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc         # zshrc
+ln -sf ~/dotfiles/bin ~/                     # scripts
+ln -sf ~/dotfiles/vimrc ~/.vimrc             # vimrc 
+# ln -sf ~/dotfiles/git/.gitconfig ~/      	 # gitconfig
+ 
+# installing powerlevel 10k
+if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh}/themes/powerlevel10k ]
+then
+	echo "Installing powerlevel 10k..."
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh}/themes/powerlevel10k
+else
+	echo "powerlevel 10k already installed"
+fi
+
+# installing lightline
+if [ ! -d ~/.vim/pack/plugins/start/lightline ]
+then
+	echo "Installing Lightline..."
+	git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
+else
+	echo "lightline already installed"
+fi
+ 
